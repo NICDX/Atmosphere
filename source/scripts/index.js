9de97c6a488e41d64d49778ef2b72a1c7c7f1c81
NICDX <119955722+NICDX@users.noreply.github.com>
2024-01-30 00:00:36 +0300

    swipers

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+import Swiper from 'swiper/bundle';
+
+//import 'swiper/css';
+
+const projectflip = new Swiper('.projectflip',{
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const teamswiper = new Swiper('.teamswiper',{
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.second',
+    prevEl: '.third',
+  }
+});
